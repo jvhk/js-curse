d375ea1298e1e682acdacfb8f87aa02ad401fe2f
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-11-01 02:52:36 -0300

    mais sobre tipos de dados

---
+++ tipos_de_dados/4_string/script.js
@@ -0,0 +1,5 @@
+console.log(typeof('Isso é uma string'));
+console.log(typeof("Queria um carro\n e uma moto"));
+console.log(typeof('Teste'));
+console.log(`A soma de 2+2 é ${2+2}`);
+console.log("O" + " meu " + "nome" + " é " + "João");
