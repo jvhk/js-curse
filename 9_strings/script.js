14fdc875b8e58d27f06607974e4688e25ebbfb56
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-11-30 02:55:16 -0300

    mais metodos para strings

--- 9_strings/script.js
+++ 9_strings/script.js
@@ -1,3 +1,15 @@
 //Métodos para string: trim
 let texto = '  \n teste \n  ';
 console.log(texto.trim()); //remove os espaços em volta da string
+
+
+let num = "40";
+console.log(num.padStart(6,"0"));
+
+
+let frase = "Bom dia Javascript"
+let palavras =  frase.split(" ");
+console.log(palavras);
+
+
+console.log(palavras.join(" "));
