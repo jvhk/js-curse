4c7747ab8d524a08409a58b92972a36433efe602
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-12-01 20:06:50 -0300

    mais sobre metodos de string e JSON

--- 9_strings/script.js
+++ 9_strings/script.js
@@ -13,3 +13,54 @@ console.log(palavras);
 
 
 console.log(palavras.join(" "));
+
+console.log(num.repeat(5));
+
+//OPERADOR REST
+function printNumbers(...args){
+    for(let i=0; i<args.length;i++){
+        console.log(args[i]);
+    }
+}
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num5 = 5;
+
+printNumbers(num1,num2,num3,num5);
+
+//MÉTODO DESTRUCTING EM OBJETOS
+const person = {
+    name: 'Jhon',
+    lastname: 'Swain'
+}
+
+const {name: fname, lastname: lname} = person;
+
+console.log(fname);
+console.log(lname);
+
+
+//MÉTODO DESTRUCTING EM ARRAYS
+let names = ['Marcos','João','José'];
+
+let [nomeA,nomeB,nomeC] = names;
+
+console.log(nomeA);
+console.log(nomeB);
+console.log(nomeC);
+
+
+//JSON:
+
+let pessoa1 = {
+    "nome" : "João",
+    "idade" : "20",
+    "profissao" : "Estudante",
+    "hobbies" : ["Video game","Leitura","Carros"]
+}
+
+let pessoaTexto = JSON.stringify(pessoa1);
+
+console.log(pessoaTexto);
