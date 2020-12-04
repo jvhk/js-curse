f06a812eb991a11f3bb0918d6cc06a6fd089e65a
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-12-04 20:44:47 -0300

    prototypes em JS

--- 11_OO/script.js
+++ 11_OO/script.js
@@ -10,13 +10,31 @@ const cachorro = {
         this.raca = raca;
     },
     getRaca: function(){
-        return "A raça é: " . this.raca;
+        return "A raça é: " + this.raca;
     }
 }
 
 
-cachorro.rosnar();
-cachorro.uivar();
+//cachorro.rosnar();
+//cachorro.uivar();
 
 cachorro.setRaca('Bulldog');
 console.log(cachorro.getRaca());
+
+
+//prototypes
+
+let pessoa = {
+    maos: 2
+}
+
+console.log(Object.getPrototypeOf(pessoa));
+console.log(Object.getPrototypeOf(pessoa) == Object.prototype);
+
+console.log(pessoa.hasOwnProperty('maos'));
+
+
+let pessoaNova = Object.create(pessoa);
+
+console.log(pessoaNova.maos);
+console.log(Object.getPrototypeOf(pessoaNova) == pessoa);
