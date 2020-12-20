216dfe087fd0bef6c29dbda3e7aed004d7a27dde
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-12-19 22:23:44 -0300

    remoção de eventos e propagação de eventos

--- 16_eventosJS/js/script.js
+++ 16_eventosJS/js/script.js
@@ -1,5 +1,45 @@
+function msg(){
+    console.log("Clicou");
+}
+
+//Criando interação com eventos
+
 let btn = document.querySelector("#btn");
 
-btn.addEventListener("click", function(){
-    console.log("Clicou");
+btn.addEventListener("click", msg);
+
+//Removendo eventos
+
+let btn2 = document.querySelector("#btn2");
+
+
+btn2.addEventListener("click",function(){
+    btn.removeEventListener("click", msg);    
+});
+
+
+//Objeto do evento
+
+function msg2(e){
+    console.log(e);
+}
+
+btn3.addEventListener("click",msg2);
+
+
+//Problema de Propagação
+
+
+function msg3(e){
+    console.log("Clicou no botão")
+    e.stopPropagation();
+}
+
+let p = document.querySelector("p");
+let btn4 = document.querySelector("#btn4");
+
+btn4.addEventListener("click",msg3);
+
+p.addEventListener("click", function(){
+    console.log("Clicou no paragrafo");
 });
