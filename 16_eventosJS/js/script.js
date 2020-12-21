8a8597ac746c6d8151a71d203328d510c7b5ab7f
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-12-21 15:28:17 -0300

    mais sobre eventos

--- 16_eventosJS/js/script.js
+++ 16_eventosJS/js/script.js
@@ -62,3 +62,79 @@ window.addEventListener("keydown",function(e){
     }
 });
 
+window.addEventListener("keyup",function(e){
+    if(e.key == "Enter"){
+        console.log("Soltou o enter");
+    }
+});
+
+
+
+//Mais sobre eventos de mouse
+
+let bntDoubleClick = document.querySelector("#btnDC");
+
+bntDoubleClick.addEventListener("dblclick", function(){
+    console.log("Ativou o double click");
+});
+
+bntDoubleClick.addEventListener("mousedown", function(){
+    console.log("Mousedown ativado");
+});
+
+bntDoubleClick.addEventListener("mouseup", function(){
+    console.log("Mouseup ativado");
+});
+
+bntDoubleClick.addEventListener("contextmenu", function(){
+    console.log("Botão direito do mouse ativado");
+});
+
+
+
+
+
+//Movimentos do mouse (deixei desabilitado pois sobrecarrega memória)
+
+/*
+window.addEventListener("mousemove", function(e){
+    console.log(e.x);
+    console.log(e.y);
+});
+
+*/
+
+
+// Eventos de scroll
+
+window.addEventListener("scroll",function(e){
+    if(window.pageYOffset > 100){
+        console.log("Chegou na posição");
+    }
+})
+
+
+// Eventos por foco
+
+let input = document.querySelector("input");
+
+input.addEventListener("focus",function(){
+    console.log("Foco no input");
+});
+
+input.addEventListener("blur",function(){
+    console.log("Perdeu o foco");
+});
+
+
+
+
+// Evento de carregamento
+
+window.addEventListener("load", function(e){
+    alert("Pressione 'ok' para carregar a tela")
+});
+
+window.addEventListener("beforeunload", function(e){
+    
+});
