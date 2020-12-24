27a24995540241e4b7da369d185c75eb5923df19
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-12-24 20:44:56 -0300

    lógica das condições de vitoria para dois jogadores

--- 17_projeto_jogo_da_velha/js/script.js
+++ 17_projeto_jogo_da_velha/js/script.js
@@ -29,6 +29,9 @@ for (let i = 0; i < boxes.length; i++) {
             }else{
                 player2++;
             }
+
+        //checa quem venceu
+        checkWinCondidtion();
         }   
 
     });   
@@ -45,4 +48,121 @@ function checkEl(player1, player2){
             el = o;
         }
     return el;
+}
+
+
+function checkWinCondidtion(){
+    let b1 = document.getElementById("block-1");
+    let b2 = document.getElementById("block-2");
+    let b3 = document.getElementById("block-3");
+    let b4 = document.getElementById("block-4");
+    let b5 = document.getElementById("block-5");
+    let b6 = document.getElementById("block-6");
+    let b7 = document.getElementById("block-7");
+    let b8 = document.getElementById("block-8");
+    let b9 = document.getElementById("block-9");
+
+    //horizontais
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0  && b3.childNodes.length > 0){
+        let b1Child = b1.childNodes[0].className;
+        let b2Child = b2.childNodes[0].className;
+        let b3Child = b3.childNodes[0].className;
+        if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x'){
+            //x
+        }else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o'){
+            //o
+        }
+    }
+
+    if(b4.childNodes.length > 0 && b5.childNodes.length > 0  && b6.childNodes.length > 0){
+        let b4Child = b4.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+        if(b4Child == 'x' && b5Child == 'x' && b6Child == 'x'){
+            //x
+        }else if(b4Child == 'o' && b5Child == 'o' && b6Child == 'o'){
+            //o
+        }
+    }
+
+    if(b7.childNodes.length > 0 && b8.childNodes.length > 0  && b9.childNodes.length > 0){
+        let b7Child = b7.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+        if(b7Child == 'x' && b8Child == 'x' && b9Child == 'x'){
+            //x
+        }else if(b7Child == 'o' && b8Child == 'o' && b9Child == 'o'){
+            //o
+        }
+    }
+
+    //verticais
+    if(b1.childNodes.length > 0 && b4.childNodes.length > 0  && b7.childNodes.length > 0){
+        let b1Child = b1.childNodes[0].className;
+        let b4Child = b4.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+        if(b1Child == 'x' && b4Child == 'x' && b7Child == 'x'){
+            //x
+        }else if(b1Child == 'o' && b4Child == 'o' && b7Child == 'o'){
+            //o
+        }
+    }
+
+    if(b2.childNodes.length > 0 && b5.childNodes.length > 0  && b8.childNodes.length > 0){
+        let b2Child = b2.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+        if(b2Child == 'x' && b5Child == 'x' && b8Child == 'x'){
+            //x
+        }else if(b2Child == 'o' && b5Child == 'o' && b8Child == 'o'){
+            //o
+        }
+    }
+
+    if(b3.childNodes.length > 0 && b6.childNodes.length > 0  && b9.childNodes.length > 0){
+        let b3Child = b3.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+        if(b3Child == 'x' && b6Child == 'x' && b9Child == 'x'){
+            //x
+        }else if(b3Child == 'o' && b6Child == 'o' && b9Child == 'o'){
+            //o
+        }
+    }
+
+    //diagonal
+    if(b1.childNodes.length > 0 && b5.childNodes.length > 0  && b9.childNodes.length > 0){
+        let b1Child = b1.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+        if(b1Child == 'x' && b5Child == 'x' && b9Child == 'x'){
+            //x
+        }else if(b1Child == 'o' && b5Child == 'o' && b9Child == 'o'){
+            //o
+        }
+    }
+
+    if(b3.childNodes.length > 0 && b5.childNodes.length > 0  && b7.childNodes.length > 0){
+        let b3Child = b3.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+        if(b3Child == 'x' && b5Child == 'x' && b7Child == 'x'){
+            //x
+        }else if(b3Child == 'o' && b5Child == 'o' && b7Child == 'o'){
+            //o
+        }
+    }
+
+    //deu velha
+    let cont = 0;
+    for (let i = 0; i < boxes.length; i++) {
+        if(boxes[i].childNodes[0] != undefined){
+            cont++;
+        }
+        if(cont == 9){
+            console.log("Deu velha!");
+        }
+        
+    }
+
 }
