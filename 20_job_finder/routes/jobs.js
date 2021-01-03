9dc36f32b3c5cbfdf7cc8aea6aa5a86899040c3c
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-02 21:47:43 -0300

    handlebar da view e css da mesma

--- 20_job_finder/routes/jobs.js
+++ 20_job_finder/routes/jobs.js
@@ -4,6 +4,21 @@ const router  = express.Router();
 const Job     = require('../models/Job');
 
 
+// detalhe da vaga -> view/1 view/2 ..
+router.get('/view/:id', (req, resp) => Job.findOne({
+        
+        where: {id: req.params.id}
+
+    }).then(job => {
+
+        resp.render('view', {
+            job})
+    
+    }).catch(err => console.log(err))
+);
+
+//form da rota de envio
+
 router.get('/add',(req, resp)=>{
     resp.render('add');
 });
